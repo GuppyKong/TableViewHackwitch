f08fc665f423043047bf4649242006e1f3f20caa
ALEJANDRO MORENO <rl106957@rlas-macair-36.local>
2018-10-29 12:09:34 -0500

    Initial Commit

---
+++ TableViewHackwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewHackwitch
+//
+//  Created by ALEJANDRO MORENO on 10/29/18.
+//  Copyright © 2018 ALEJANDRO MORENO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
